8a09f99331f20548fe0819dac49e184517fde74e
Om <144691499+ombalgude@users.noreply.github.com>
2025-07-17 20:32:11 +0530

    changed balance component ot show remaining amount

--- frontend/src/components/Balance.jsx
+++ frontend/src/components/Balance.jsx
@@ -1,12 +1,12 @@
 import axios from "axios";
 import BACKEND_URL from "../../config";
 
-export const Balance = () => {
-  const balance = axios.get(`${BACKEND_URL/api/v1/account/balance}`)
+export default async function Balance(){
+  const amount = await axios.get(`${BACKEND_URL}/api/v1/account/balance}`)
   return (
     <div className="flex items-center justify-between bg-gray-900 border border-gray-800 rounded-xl px-6 py-4 shadow-sm">
       <div className="text-blue-400 font-semibold text-lg">Your Balance</div>
-      <div className="text-white font-bold text-2xl tracking-wide">₹ {balance} </div>
+      <div className="text-white font-bold text-2xl tracking-wide">₹ {amount} </div>
     </div>
   );
 };
